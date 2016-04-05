74c6ff0aec36e26d6193de40f64ae217fc8a57e1
Danny Alvarez <dannynalvarez@gmail.com>
2016-04-04 23:23:09 -0700

    greedy pagination ftw - grab all pages and cache them

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var sassMiddleware = require('node-sass-middleware');
-var questionsDB = require('./modules/questions');
+let questionsDB = require('./modules/questions');
 let options = {
   maxRecords: 1000,
-  pageSize: 10
+  pageSize: 20
 };
+var templateFile = fs.readFileSync(__dirname + '/views/partials/questions.ejs', 'utf8');
+
+let questionsTemplate = ejs.compile(templateFile);
+let newHTML = '';
 
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
@@ -23,89 +27,48 @@ app.use(sassMiddleware({
     outputStyle: 'compressed',
     prefix:  '/public/styles/css'
 }));
-app.locals.fetchNextPage = null;
-app.locals.pages = {};
-// app.locals.currentPage = 1;
 
-var templateFile = fs.readFileSync(__dirname + '/views/partials/questions.ejs', 'utf8');
+questionsDB.all(options, function(error, pages){
+  console.log('All of the questions are cached.');
+});
 
-let questionsTemplate = ejs.compile(templateFile);
-let newHTML = '';
+app.get('/', function(request, response){
+  console.log('initial rendering');
+  response.render('pages/index', {questions: undefined, questionsTemplate: questionsTemplate});
+}); // END of app.get '/'
 
 io.on('connection', function(client){
   console.log('Client connected...');
-  let currentPage = 1;
-  // let pages = {};
-  // let fetchNextPage = null;
+  let currentPageNumber = 1;
+  // let currentPage = null;
 
-
-  let sendNextPage = function(){
-    console.log('sending the page to the client');
-    let showNextButton = app.locals.pages[currentPage + 1] !== undefined ? true : false;
-    console.log('showNextButton?', showNextButton);
-    newHTML = questionsTemplate({questions: app.locals.pages[currentPage]});
-    client.emit('nextPage', {questions: newHTML, currentPage: currentPage, showNextButton: showNextButton});
+  let sendPage = function(page, showPrevious, showNext){
+    console.log('sendPage was called');
+    newHTML = questionsTemplate({questions: page});
+    client.emit('newPage', {questionsHTML: newHTML, showPrevious: showPrevious, showNext: showNext});
   };
 
-  let cacheResults = function(questions, pageNumber){
-    console.log('caching questions');
-    app.locals.pages[pageNumber] = questions;
-    if(app.locals.pages[currentPage] !== undefined){
-      console.log('grabbing next page from cache');
-      sendNextPage();
-    }
-    if(app.locals.pages[currentPage + 1] === undefined
-    && app.locals.fetchNextPage !== null ){
-      //need to pre-fetch the next page
-      app.locals.fetchNextPage();
-    }
-    else {
-      if(currentPage === 1)
-        sendNextPage();
-    }
+  let getPage = function(currentPageNumber){
+    questionsDB.getPage(currentPageNumber, function(data){
+      sendPage(data.page, data.showPrevious, data.showNext);
+    });
   };
 
   client.on('nextPage', function(){
-    //need to make sure that this will be unique to each client
-    currentPage++;
-    if(app.locals.pages[currentPage + 1] === undefined
-    && app.locals.fetchNextPage !== null ){
-      console.log('calling fetchNextPage');
-      app.locals.fetchNextPage();
-    } else {
-      if(app.locals.pages[currentPage] !== undefined){
-        console.log('grabbing next page from cache');
-        sendNextPage();
-      }
-    }
-
-
+    console.log('nextPage was called');
+    currentPageNumber = currentPageNumber + 1;
+    getPage(currentPageNumber);
   });
 
   client.on('previousPage', function(){
-    currentPage--;
-    newHTML = questionsTemplate({questions: app.locals.pages[currentPage]});
-    client.emit('previousPage', {questions: newHTML, currentPage: currentPage, showNextButton: true});
+    console.log('previousPage was called');
+    currentPageNumber = currentPageNumber - 1;
+    getPage(currentPageNumber);
   });
 
-  questionsDB.all(options, function(error, questions, pageNumber, nextPageFn){
-    console.log('questionsDB callback here');
-    console.log('current page is ', currentPage);
-    if(nextPageFn === null){
-      //no more pages
-      console.log('database has no more pages');
-      sendNextPage();
-    } else {
-      //maybe do this if not null, only
-      app.locals.fetchNextPage = nextPageFn;
-      cacheResults(questions, pageNumber);
-      // sendNextPage();
-    }
-  });
+  getPage(currentPageNumber);
 });
-app.get('/', function(request, response){
-  response.render('pages/index', {questions: undefined, questionsTemplate: questionsTemplate});
-}); // END of app.get '/'
+
 
 
 server.listen('3000', function(){
--- modules/questions.js
+++ modules/questions.js
@@ -1,12 +1,29 @@
 "use strict";
 
 var utils = require('./utils');
-var async = require('async');
 var Airtable = require('airtable');
 var base = new Airtable({ apiKey: 'keyDSIKXybboB4yTY' }).base('appfeRWL1dYhKSR9E');
 let pageNumber = 0;
+
 var questions = {
+  pages: {},
+
+  getPage: function(number, callback){
+    console.log('getPage was called');
+    let that = this;
+    let data = {};
+    if(that.pages[number] !== undefined){
+      data.page = that.pages[number];
+    }
+    data.showNext = that.pages[number + 1] !== undefined;
+    data.showPrevious = that.pages[number - 1] !== undefined;
+    callback(data);
+  },
+
   all: function(options, doneCallback){
+    let that = this;
+    let pages = this.pages;
+
     options = options || {};
 
     var defaultOptions = {
@@ -16,58 +33,62 @@ var questions = {
     };
 
     options = utils.extend(defaultOptions, options);
-        base('Questions')
-        .select(options)
-        .eachPage(function page(records, fetchNextPage){
-          var questions = [];
-          var question;
-          pageNumber++;
-          records.forEach(function(record) {
-            question = {
-              choices: {}
-            };
-            question.text = record.get('text');
-            question.difficulty = record.get('difficulty')[0];
-            question.topics = record.get('topics');
-            question.questionNumber = record.get('questionNumber');
-            if (record.get('questionImage')) {
-              question.image = record.get('questionImage')[0].url;
-            }
-            question.choices.choiceA = utils.extend({}, {
-                text: record.get('choiceAText'),
-                image: record.get('choiceAImage') ? record.get('choiceAImage')[0].url : undefined,
-            });
-            question.choices.choiceB = utils.extend({}, {
-                text: record.get('choiceBText'),
-                image: record.get('choiceBImage') ? record.get('choiceBImage')[0].url : undefined,
-            });
-            question.choices.choiceC = utils.extend({}, {
-                text: record.get('choiceCText'),
-                image: record.get('choiceCImage') ? record.get('choiceCImage')[0].url : undefined,
-            });
-            question.choices.choiceD = utils.extend({}, {
-                text: record.get('choiceDText'),
-                image: record.get('choiceDImage') ? record.get('choiceDImage')[0].url : undefined,
-            });
-            question.choices.choiceE = utils.extend({}, {
-                text: record.get('choiceEText'),
-                image: record.get('choiceEImage') ? record.get('choiceEImage')[0].url : undefined,
-            });
 
-            questions.push(question);
-          });
-          // waterfallCallback(null, questions, fetchNextPage);
-          doneCallback(null, questions, pageNumber, fetchNextPage);
+    base('Questions')
+    .select(options)
+    .eachPage(function page(records, fetchNextPage){
+      var questions = [];
+      var question;
+      pageNumber++;
+      records.forEach(function(record) {
+        question = {
+          choices: {}
+        };
+        question.text = record.get('text');
+        if(record.get('difficulty')){
+          question.difficulty = record.get('difficulty')[0];
+        }
+        question.topics = record.get('topics');
+        question.questionNumber = record.get('questionNumber');
+        if (record.get('questionImage')) {
+          question.image = record.get('questionImage')[0].url;
+        }
+        question.choices.choiceA = utils.extend({}, {
+            text: record.get('choiceAText'),
+            image: record.get('choiceAImage') ? record.get('choiceAImage')[0].url : undefined,
+        });
+        question.choices.choiceB = utils.extend({}, {
+            text: record.get('choiceBText'),
+            image: record.get('choiceBImage') ? record.get('choiceBImage')[0].url : undefined,
+        });
+        question.choices.choiceC = utils.extend({}, {
+            text: record.get('choiceCText'),
+            image: record.get('choiceCImage') ? record.get('choiceCImage')[0].url : undefined,
+        });
+        question.choices.choiceD = utils.extend({}, {
+            text: record.get('choiceDText'),
+            image: record.get('choiceDImage') ? record.get('choiceDImage')[0].url : undefined,
+        });
+        question.choices.choiceE = utils.extend({}, {
+            text: record.get('choiceEText'),
+            image: record.get('choiceEImage') ? record.get('choiceEImage')[0].url : undefined,
+        });
 
-          // fetchNextPage();
-        }, function done(error){
-          if(error){
-            console.log('something went wrong fetching questions ', error);
-            //callback(error);
-          }
-            console.log('no more records to fetch');
-            doneCallback(null, [], null, null);
-          }); //end of eachPage()
+        questions.push(question);
+      });
+      console.log('questions for page '+pageNumber+' gotten. caching and storing fetch');
+      //cache the pages at this level
+      pages[pageNumber] = questions;
+
+      fetchNextPage();
+    }, function done(error){
+      if(error){
+        console.log('something went wrong fetching questions ', error);
+        //callback(error);
+      }
+        console.log('no more records to fetch');
+        doneCallback(null, pages);
+      }); //end of eachPage()
 
   }
 };
